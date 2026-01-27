9a255a0a79199c2a1a56ef7429faf3468cb2c0e4
Muhammad Al Faiz <faiz.burham@gmail.com>
2026-01-27 15:04:46 +0700

    [MAF] add Update on database

--- lib/Components/custom_card.dart
+++ lib/Components/custom_card.dart
@@ -1,215 +1,138 @@
 import 'package:flutter/material.dart';
+import 'package:tugas1_11pplg2/Models/menu_makanan_model.dart';
 
 class MenuCard extends StatelessWidget {
   final String menuTitle;
   final String description;
   final String price;
-  final int stock;
-  final VoidCallback? onAddPressed;
+  final VoidCallback? onEditPressed;
   final VoidCallback? onDeletePressed;
   final String? imageUrl;
+  final ItemMakanan? makanan;
 
   const MenuCard({
     super.key,
     required this.menuTitle,
     required this.description,
     required this.price,
-    required this.stock,
-    this.onAddPressed,
+    this.onEditPressed,
     this.onDeletePressed,
     this.imageUrl,
+    this.makanan,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
       height: 163,
-      margin: const EdgeInsets.symmetric(horizontal:2),
-      clipBehavior: Clip.antiAlias,
-      decoration: ShapeDecoration(
+      decoration: BoxDecoration(
         color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(18),
-        ),
-        shadows: const [
+        borderRadius: BorderRadius.circular(18),
+        boxShadow: const [
           BoxShadow(
             color: Color(0x3F000000),
             blurRadius: 10,
             offset: Offset(2, 2),
-            spreadRadius: 0,
-          )
+          ),
         ],
       ),
       child: Stack(
         children: [
+          /// IMAGE
           Positioned(
             left: 16,
             top: 32,
-            child: Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                shape: BoxShape.circle,
-                image: imageUrl != null
-                    ? DecorationImage(
-                        image: NetworkImage(imageUrl!),
-                        fit: BoxFit.cover,
-                      )
-                    : null,
-              ),
+            child: CircleAvatar(
+              radius: 50,
+              backgroundImage: imageUrl != null
+                  ? NetworkImage(imageUrl!)
+                  : null,
+              backgroundColor: Colors.grey[300],
               child: imageUrl == null
-                  ? Icon(
-                      Icons.restaurant,
-                      size: 50,
-                      color: Colors.grey[500],
-                    )
+                  ? const Icon(Icons.restaurant, size: 40)
                   : null,
             ),
           ),
 
+          /// TEXT
           Positioned(
             left: 130,
             top: 42,
-            right: 100,
+            right: 80,
             child: Column(
-              mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   menuTitle,
                   style: const TextStyle(
-                    color: Color(0xFF121212),
                     fontSize: 16,
                     fontWeight: FontWeight.w600,
-                    height: 1.2,
                   ),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
                 ),
                 const SizedBox(height: 6),
-                Opacity(
-                  opacity: 0.70,
-                  child: Text(
-                    description,
-                    style: const TextStyle(
-                      color: Color(0xFF121212),
-                      fontSize: 12,
-                      fontWeight: FontWeight.w400,
-                      height: 1.30,
-                    ),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
+                Text(
+                  description,
+                  style: const TextStyle(fontSize: 12, color: Colors.grey),
                 ),
                 const SizedBox(height: 8),
                 Text(
                   price,
                   style: const TextStyle(
-                    color: Color(0xFF483649),
                     fontSize: 18,
-                    fontWeight: FontWeight.w700,
-                    height: 1,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF483649),
                   ),
                 ),
               ],
             ),
           ),
 
+          /// EDIT (KANAN ATAS)
           Positioned(
             right: 16,
             top: 16,
-            child: Container(
-              height: 28,
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
-              decoration: ShapeDecoration(
-                color: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                shadows: const [
-                  BoxShadow(
-                    color: Color(0x1A000000),
-                    blurRadius: 4,
-                    offset: Offset(0, 1),
-                    spreadRadius: 0,
-                  )
-                ],
-              ),
-              child: Text(
-                'Stock: $stock',
-                style: const TextStyle(
-                  color: Colors.black87,
-                  fontSize: 12,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ),
+            child: _icon(Icons.edit, onEditPressed),
           ),
 
-          Positioned(
-            right: 56,
-            bottom: 16,
-            child: GestureDetector(
-              onTap: onAddPressed,
-              child: Container(
-                width: 32,
-                height: 32,
-                decoration: ShapeDecoration(
-                  color: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  shadows: const [
-                    BoxShadow(
-                      color: Color(0x26000000),
-                      blurRadius: 4,
-                      offset: Offset(0, 2),
-                      spreadRadius: 0,
-                    )
-                  ],
-                ),
-                child: const Icon(
-                  Icons.add,
-                  size: 22,
-                  color: Colors.black87,
-                ),
-              ),
-            ),
-          ),
-
+          /// DELETE (KANAN BAWAH)
           Positioned(
             right: 16,
             bottom: 16,
-            child: GestureDetector(
-              onTap: onDeletePressed,
-              child: Container(
-                width: 32,
-                height: 32,
-                decoration: ShapeDecoration(
-                  color: const Color(0xFFC04747),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  shadows: const [
-                    BoxShadow(
-                      color: Color(0x26000000),
-                      blurRadius: 4,
-                      offset: Offset(0, 2),
-                      spreadRadius: 0,
-                    )
-                  ],
-                ),
-                child: const Icon(
-                  Icons.delete_outline,
-                  size: 20,
-                  color: Colors.white,
-                ),
-              ),
+            child: _icon(
+              Icons.delete,
+              onDeletePressed,
+              bg: const Color(0xFFC04747),
+              color: Colors.white,
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _icon(
+    IconData icon,
+    VoidCallback? onTap, {
+    Color bg = Colors.white,
+    Color color = Colors.black87,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 32,
+        height: 32,
+        decoration: BoxDecoration(
+          color: bg,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: const [
+            BoxShadow(
+              color: Color(0x26000000),
+              blurRadius: 4,
+              offset: Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Icon(icon, size: 18, color: color),
       ),
     );
   }
